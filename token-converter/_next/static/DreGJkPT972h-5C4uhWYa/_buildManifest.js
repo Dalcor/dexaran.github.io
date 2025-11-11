f02dbce65ddc47b4b89af09e6c5d2be50352e069
Viktor Peredera <vr.peredera@gmail.com>
2025-11-11 23:38:35 +0200

    fix: update converter #4

--- token-converter/_next/static/DreGJkPT972h-5C4uhWYa/_buildManifest.js
+++ token-converter/_next/static/DreGJkPT972h-5C4uhWYa/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/token-converter//_next/:path+",
+        "destination": "/token-converter/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
