ea7359c88e549435462454a91a35db311df82b1a
Viktor Peredera <vr.peredera@gmail.com>
2025-11-11 23:33:05 +0200

    fix: update converter

--- token-converter-sources/next.config.ts
+++ token-converter-sources/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/token-converter",
-  assetPrefix: "/token-converter",
+  assetPrefix: "/",
   /* config options here */
 };
 
